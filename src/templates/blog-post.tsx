7e44b424d2dc14199d644ad6230774c25765eda9
Serge Krstic <srdjankrstic@gmail.com>
2021-04-18 21:32:21 +1000

    Add disqus comments to blog post layout

--- src/templates/blog-post.tsx
+++ src/templates/blog-post.tsx
@@ -1,124 +1,118 @@
-import React from "react"
-import { Link, graphql } from "gatsby"
+import React from 'react';
+import { Link, graphql } from 'gatsby';
+import { DiscussionEmbed } from 'disqus-react';
 
-import Bio from "../components/Bio"
-import Layout from "../components/Layout"
-import SEO from "../components/Seo"
+import Bio from '../components/Bio';
+import Layout from '../components/Layout';
+import SEO from '../components/Seo';
 
-import "katex/dist/katex.min.css"
+import 'katex/dist/katex.min.css';
 
 interface Props {
-  data: {
-    markdownRemark: any
-    next: any
-    previous: any
-    site: {
-      siteMetadata: {
-        title: string
-      }
-    }
-  }
-  pageContext: any
-  location: any
+    data: {
+        markdownRemark: any;
+        next: any;
+        previous: any;
+        site: {
+            siteMetadata: {
+                title: string;
+            };
+        };
+    };
+    pageContext: any;
+    location: any;
 }
 
 const BlogPostTemplate = ({ data, location }: Props) => {
-  const post = data.markdownRemark
-  const siteTitle = data.site.siteMetadata?.title || `Title`
-  const { previous, next } = data
+    const post = data.markdownRemark;
+    const siteTitle = data.site.siteMetadata?.title || `Title`;
+    const { previous, next } = data;
 
-  return (
-    <Layout location={location} title={siteTitle}>
-      <SEO
-        title={post.frontmatter.title}
-        description={post.frontmatter.description || post.excerpt}
-      />
-      <article
-        className="blog-post"
-        itemScope
-        itemType="http://schema.org/Article"
-      >
-        <header>
-          <h1 itemProp="headline">{post.frontmatter.title}</h1>
-          <p>{post.frontmatter.date}</p>
-        </header>
-        <section
-          dangerouslySetInnerHTML={{ __html: post.html }}
-          itemProp="articleBody"
-        />
-        <hr />
-        <footer>
-          <Bio />
-        </footer>
-      </article>
-      <nav className="blog-post-nav">
-        <ul
-          style={{
-            display: `flex`,
-            flexWrap: `wrap`,
-            justifyContent: `space-between`,
-            listStyle: `none`,
-            padding: 0,
-          }}
-        >
-          <li>
-            {previous && (
-              <Link to={`/${previous.fields.slug}`} rel="prev">
-                ← {previous.frontmatter.title}
-              </Link>
-            )}
-          </li>
-          <li>
-            {next && (
-              <Link to={`/${next.fields.slug}`} rel="next">
-                {next.frontmatter.title} →
-              </Link>
-            )}
-          </li>
-        </ul>
-      </nav>
-    </Layout>
-  )
-}
+    const disqusConfig = {
+        shortname: process.env.GATSBY_DISQUS_NAME || '',
+        // config: { identifier: slug, title: siteTitle },
+        config: { identifier: siteTitle, title: siteTitle },
+    };
 
-export default BlogPostTemplate
+    return (
+        <Layout location={location} title={siteTitle}>
+            <SEO title={post.frontmatter.title} description={post.frontmatter.description || post.excerpt} />
+            <article className="blog-post" itemScope itemType="http://schema.org/Article">
+                <header>
+                    <h1 itemProp="headline">{post.frontmatter.title}</h1>
+                    <p>{post.frontmatter.date}</p>
+                </header>
+                <section dangerouslySetInnerHTML={{ __html: post.html }} itemProp="articleBody" />
+                <hr />
+                <DiscussionEmbed {...disqusConfig} />
+                <footer>
+                    <Bio />
+                </footer>
+            </article>
+            <nav className="blog-post-nav">
+                <ul
+                    style={{
+                        display: `flex`,
+                        flexWrap: `wrap`,
+                        justifyContent: `space-between`,
+                        listStyle: `none`,
+                        padding: 0,
+                    }}
+                >
+                    <li>
+                        {previous && (
+                            <Link to={`/${previous.fields.slug}`} rel="prev">
+                                ← {previous.frontmatter.title}
+                            </Link>
+                        )}
+                    </li>
+                    <li>
+                        {next && (
+                            <Link to={`/${next.fields.slug}`} rel="next">
+                                {next.frontmatter.title} →
+                            </Link>
+                        )}
+                    </li>
+                </ul>
+            </nav>
+        </Layout>
+    );
+};
+
+export default BlogPostTemplate;
 
 export const pageQuery = graphql`
-  query BlogPostBySlug(
-    $id: String!
-    $previousPostId: String
-    $nextPostId: String
-  ) {
-    site {
-      siteMetadata {
-        title
-      }
+    query BlogPostBySlug($id: String!, $previousPostId: String, $nextPostId: String) {
+        site {
+            siteMetadata {
+                title
+            }
+        }
+        markdownRemark(id: { eq: $id }) {
+            id
+            excerpt(pruneLength: 160)
+            html
+            frontmatter {
+                title
+                date(formatString: "MMMM DD, YYYY")
+                description
+            }
+        }
+        previous: markdownRemark(id: { eq: $previousPostId }) {
+            fields {
+                slug
+            }
+            frontmatter {
+                title
+            }
+        }
+        next: markdownRemark(id: { eq: $nextPostId }) {
+            fields {
+                slug
+            }
+            frontmatter {
+                title
+            }
+        }
     }
-    markdownRemark(id: { eq: $id }) {
-      id
-      excerpt(pruneLength: 160)
-      html
-      frontmatter {
-        title
-        date(formatString: "MMMM DD, YYYY")
-        description
-      }
-    }
-    previous: markdownRemark(id: { eq: $previousPostId }) {
-      fields {
-        slug
-      }
-      frontmatter {
-        title
-      }
-    }
-    next: markdownRemark(id: { eq: $nextPostId }) {
-      fields {
-        slug
-      }
-      frontmatter {
-        title
-      }
-    }
-  }
-`
+`;
